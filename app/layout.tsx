fb4eb24d450b17defa899a174e245e2323838fe2
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2023-01-05 09:23:54 +0700

    Navigation with Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,30 @@
+import Link from "next/link";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link href="/settings">Settings</Link>
+          </li>
+        </ul>
+
+        {children}
+
+        <div>Footer</div>
+      </body>
     </html>
-  )
+  );
 }
