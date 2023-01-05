0313bcd7a3f807c752e9ae9ef7bd8dcc37212acf
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2023-01-05 11:24:55 +0700

    Server Component Fetching Data

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -4,8 +4,22 @@ type Props = {
   params: { slug: string };
 };
 
-const SearchDetails = ({ params }: Props) => {
-  return <div>Details User: {params.slug}</div>;
+const getData = async (params: string) => {
+  const res = await fetch(`https://api.github.com/users/${params}`);
+
+  return res.json();
+};
+
+const SearchDetails = async ({ params }: Props) => {
+  const data = await getData(params.slug);
+
+  return (
+    <div>
+      <p>Details User: {params.slug}</p>
+
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
 };
 
 export default SearchDetails;
