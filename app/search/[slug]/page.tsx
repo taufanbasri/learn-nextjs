3d34551b8848dbc2a32cf44737da5cbc6c62e10b
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2023-01-07 21:27:10 +0700

    Parallel Data Fetching

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -4,20 +4,34 @@ type Props = {
   params: { slug: string };
 };
 
-const getData = async (params: string) => {
+const getDataUser = async (params: string) => {
   const res = await fetch(`https://api.github.com/users/${params}`);
 
   return res.json();
 };
 
+const getDataRepos = async (params: string) => {
+  const res = await fetch(`https://api.github.com/users/${params}/repos`);
+  // contoh delay
+  await new Promise((r) => setTimeout(r, 2000));
+
+  return res.json();
+};
+
 const SearchDetails = async ({ params }: Props) => {
-  const data = await getData(params.slug);
+  const dataUser = getDataUser(params.slug);
+  const dataRepos = getDataRepos(params.slug);
+
+  const [user, repos] = await Promise.all([dataUser, dataRepos]);
 
   return (
     <div>
       <p>Details User: {params.slug}</p>
 
-      <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(user)}</div>
+
+      <p>List Repository:</p>
+      <div>{JSON.stringify(repos)}</div>
     </div>
   );
 };
